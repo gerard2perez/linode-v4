7652be20d4e5e1f090f904c1b1454b4c247f056c
Gerardo Pérez Pérez <gerard2perez@outlook.com>
2017-09-13 14:22:34 -0500

    feat: Add Support for Linodes

---
+++ src/index.js
@@ -0,0 +1,133 @@
+import { createJsonClient } from 'restify-clients';
+import spec from './spec';
+
+function response (resolve, err, req, res, obj) {
+	resolve(obj);
+}
+function appendcustom (id, actions, target, route, client) {
+	for (let custom of actions) {
+		if (custom.indexOf(':')) {
+			let ar = custom.split(':');
+			if (ar.length >= 3 && ar[0] === 'single' && id) {
+				let path = `${route}/${id}/${ar[1]}`;
+				if (ar[3] && ar[3] === 'noargs') {
+					path = `${route}/${id}}`;
+				}
+				target[ar[1]] = function (data) {
+					return new Promise(function (resolve) {
+						if (data) {
+							client[ar[2]](path, data, response.bind(null, resolve));
+						} else {
+							client[ar[2]](path, response.bind(null, resolve));
+						}
+					});
+				};
+			} else if (ar.length === 2 && !id) {
+				target[ar[0]] = function (data) {
+					return new Promise(function (resolve) {
+						if (data) {
+							client[ar[1]](`${route}/${ar[0]}`, data, response.bind(null, resolve));
+						} else {
+							client[ar[1]](`${route}/${ar[0]}`, response.bind(null, resolve));
+						}
+					});
+				};
+			}
+		}
+	}
+}
+function makeapi (route, collection) {
+	collection.actions = collection.actions || [];
+	let client = this.client;
+	let handler = (id) => {
+		let res = {};
+		if (id) {
+			if (collection.actions.indexOf('get') > -1) {
+				res.get = function () {
+					return new Promise(function (resolve) {
+						client.get(`${route}/${id}`, response.bind(null, resolve));
+					});
+				};
+			}
+			if (collection.actions.indexOf('delete') > -1) {
+				res.delete = function () {
+					return new Promise(function (resolve) {
+						client.del(`${route}/${id}`, response.bind(null, resolve));
+					});
+				};
+			}
+			if (collection.actions.indexOf('update') > -1) {
+				res.update = function (data = {}) {
+					return new Promise(function (resolve) {
+						client.put(`${route}/${id}`, data, response.bind(null, resolve));
+					});
+				};
+			}
+			appendcustom(id, collection.actions, res, route, client);
+		}
+		for (const k in collection.collections) {
+			res[k] = makeapi.bind(this)(`${route}/${id}/${k}`, collection.collections[k]);
+		}
+		return res;
+	};
+	if (collection.actions.indexOf('create') > -1) {
+		handler.create = (data) => {
+			return new Promise(function (resolve) {
+				client.post(`${route}`, data, response.bind(null, resolve));
+			});
+		};
+	}
+	if (collection.actions.indexOf('list') > -1) {
+		handler.list = () => {
+			return new Promise(function (resolve) {
+				client.get(`${route}`, response.bind(null, resolve));
+			});
+		};
+	}
+
+	if (collection.query) {
+		let root = handler;
+		let send = route;
+		for (const query of collection.query) {
+			let current;
+			root[query] = function (data) {
+				send += `/${data}`;
+				return current;
+			};
+			current = root[query];
+			root = root[query];
+		}
+		root.get = () => {
+			return new Promise(function (resolve) {
+				client.get(`${send}`, response.bind(null, resolve));
+			});
+		};
+	}
+
+	if (collection.actions.length < 2) {
+		for (const k in collection.collections) {
+			handler[k] = makeapi.bind(this)(`${route}/${k}`, collection.collections[k]);
+		}
+	}
+	appendcustom(null, collection.actions, handler, route, client);
+	return handler;
+}
+
+export default class Linode {
+	constructor (token) {
+		Object.defineProperty(this, 'client', {
+			enumerable: false,
+			writable: false,
+			configurable: false,
+			value: createJsonClient({
+				url: 'https://api.linode.com',
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			})
+		});
+		for (const key in spec) {
+			this[key] = makeapi.bind(this)('/v4/' + key, spec[key]);
+		}
+	}
+}
---
+++ test/index.js
@@ -0,0 +1,106 @@
+import Linode from '../src';
+import * as assert from 'assert';
+
+const app = new Linode('abc_token');
+
+describe('Linodes', function () {
+	it('Instances', async function () {
+		assert.ok(app.linode.instances);
+		assert.ok(app.linode.instances.list);
+		assert.ok(app.linode.instances.create);
+		assert.ok(app.linode.instances(1));
+		assert.ok(app.linode.instances(1).get);
+		assert.ok(app.linode.instances(1).update);
+		assert.ok(app.linode.instances(1).delete);
+		assert.ok(app.linode.instances(1).boot);
+		assert.ok(app.linode.instances(1).kvmify);
+		assert.ok(app.linode.instances(1).mutate);
+		assert.ok(app.linode.instances(1).reboot);
+		assert.ok(app.linode.instances(1).rebuild);
+		assert.ok(app.linode.instances(1).rescue);
+		assert.ok(app.linode.instances(1).resize);
+		assert.ok(app.linode.instances(1).shutdown);
+		assert.ok(app.linode.instances(1).volumes);
+	});
+	it('Backups', async function () {
+		assert.ok(app.linode.instances(1).backups, 'get /linode/instances/:id/backups');
+		assert.ok(app.linode.instances(1).backups.create, 'post /linode/instances/:id/backups');
+		assert.ok(app.linode.instances(1).backups(1).restore, 'post /linode/instances/:id/backups/:id/restore');
+		assert.ok(app.linode.instances(1).backups.cancel);
+		assert.ok(app.linode.instances(1).backups.enable);
+	});
+	it('Configs', async function () {
+		assert.ok(app.linode.instances(1).configs, 'get /linode/instances/:id/configs');
+		assert.ok(app.linode.instances(1).configs.create, 'post /linode/instances/:id/configs');
+		assert.ok(app.linode.instances(1).configs(1).get, 'post /linode/instances/:id/configs/:id');
+		assert.ok(app.linode.instances(1).configs(1).update, 'post /linode/instances/:id/configs/:id');
+		assert.ok(app.linode.instances(1).configs(1).delete, 'post /linode/instances/:id/configs/:id');
+	});
+	it('Disks', async function () {
+		assert.ok(app.linode.instances(1).disks, 'get /linode/instances/:id/disks');
+		assert.ok(app.linode.instances(1).disks.create, 'post /linode/instances/:id/disks');
+		assert.ok(app.linode.instances(1).disks(1).get, 'get /linode/instances/:id/disks/:id');
+		assert.ok(app.linode.instances(1).disks(1).update, 'put /linode/instances/:id/disks/:id');
+		assert.ok(app.linode.instances(1).disks(1).duplicate, 'post /linode/instances/:id/disks/:id');
+		assert.ok(app.linode.instances(1).disks(1).delete, 'del /linode/instances/:id/disks/:id');
+		assert.ok(app.linode.instances(1).disks(1).password, 'post /linode/instances/:id/disks/:id/password');
+		assert.ok(app.linode.instances(1).disks(1).resize, 'post /linode/instances/:id/disks/:id/password');
+	});
+	it('Distributions', async function () {
+		assert.ok(app.linode.distributions);
+		assert.ok(app.linode.distributions.list, 'get /linode/distributions');
+		assert.ok(app.linode.distributions(1).get, 'get /linode/distributions/:id');
+	});
+	it('IPs', async function () {
+		assert.ok(app.linode.instances(1).ips);
+		assert.ok(app.linode.instances(1).ips.list, 'get /linode/instances/:id/ips');
+		assert.ok(app.linode.instances(1).ips(1).get, 'put /linode/instances/:id/ips/:ip_address');
+		assert.ok(app.linode.instances(1).ips.sharing, 'post /linode/instances/:id/ips/sharing');
+	});
+	it('Kernels', async function () {
+		assert.ok(app.linode.kernels);
+		assert.ok(app.linode.kernels.list, 'get /linode/kernels');
+		assert.ok(app.linode.kernels(1).get, 'get /linode/kernels/:id');
+	});
+	it('StackScripts', async function () {
+		assert.ok(app.linode.stackscripts);
+		assert.ok(app.linode.stackscripts.list, 'get /linode/stackscripts');
+		assert.ok(app.linode.stackscripts.create, 'get /linode/stackscripts');
+		assert.ok(app.linode.stackscripts(1).get, 'get /linode/stackscripts/:id');
+		assert.ok(app.linode.stackscripts(1).update, 'put /linode/stackscripts/:id');
+		assert.ok(app.linode.stackscripts(1).delete, 'del /linode/stackscripts/:id');
+	});
+	it('Stats', async function () {
+		assert.ok(app.linode.instances(1).stats, 'get /linode/instances/:id/stats');
+		assert.ok(app.linode.instances(1).stats.year(1).month(1).get, 'get /linode/instances/:id/stats/:year/:month');
+	});
+	it('Types', async function () {
+		assert.ok(app.linode.types);
+		assert.ok(app.linode.types.list, 'get /linode/types');
+		assert.ok(app.linode.types(1).get, 'get /linode/types/:id');
+	});
+	it('Volumes', async function () {
+		assert.ok(app.linode.volumes);
+		assert.ok(app.linode.volumes.list, 'get /linode/volumes');
+		assert.ok(app.linode.volumes.create, 'get /linode/volumes');
+		assert.ok(app.linode.volumes(1).get, 'get /linode/volumes/:id');
+		assert.ok(app.linode.volumes(1).update, 'put /linode/volumes/:id');
+		assert.ok(app.linode.volumes(1).delete, 'del /linode/volumes/:id');
+
+		assert.ok(app.linode.volumes(1).attach, 'del /linode/volumes/:id/attach');
+		assert.ok(app.linode.volumes(1).detach, 'del /linode/volumes/:id/detach');
+	});
+});
+
+describe('Profie', function () {
+	it('Profile', function () {
+		assert.ok(app.profile.get, 'get /profile');
+		assert.ok(app.profile.grants, 'get /profile/grants');
+		assert.ok(app.profile.password, 'post /profile/grants');
+	});
+	it('Two-Factor Authentication', function () {
+		assert.ok(app.profile.tfaDisable, 'post /profile/tfa-disable');
+		assert.ok(app.profile.tfaEnable, 'post /profile/tfa-disable');
+		assert.ok(app.profile.tfaEnableConfirm, 'post /profile/tfa-enable-confirm');
+	});
+});
