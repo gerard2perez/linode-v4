8f5b62c47ac5d228f6f7a5c5e3f043b90f55fcaf
Gerardo Pérez <gerard2perez@outlook.com>
2018-04-26 17:25:26 -0500

    fix: delete methods will work correctly

--- src/extended-client.ts
+++ src/extended-client.ts
@@ -11,7 +11,8 @@ function ClientMethod(target:ExtendedClient, key:string, descriptor:PropertyDesc
     descriptor.value = function (route:string, data?:any, filter?:any) {
 		const parent:any = this;
 		return new Promise<any>(function(resolve) {
-			parent.client[key](...parent.prepare(route, data, filter), (json:any) => {
+			let mth = key === 'delete' ? 'del' : key;
+			parent.client[mth](...parent.prepare(route, data, filter), (json:any) => {
 				resolve(json);
 			});
 		});
